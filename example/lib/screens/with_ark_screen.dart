a05438a010763dcc13bda52aeae038d29939ab2e
Ahmadre <rebar.ahmad@telexiom.de>
2020-08-13 04:10:02 +0200

    Refactor

--- example/lib/screens/with_ark_screen.dart
+++ example/lib/screens/with_ark_screen.dart
@@ -21,7 +21,6 @@ class _WithARkitScreenState extends State<WithARkitScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
         key: _scaffoldKey,
         appBar: AppBar(
           title: const Text('Unity Flutter Demo'),
